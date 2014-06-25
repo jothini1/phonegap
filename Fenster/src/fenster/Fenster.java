fda0a22396e81ad44c7ea97bf7fcf57aea6e5ba8
srithj <srithj@IR1-PC-19-W7.schule.local>
2014-06-25 17:16:57 +0200

    Grundstruktur angelegt

--- Fenster/src/fenster/Fenster.java
+++
@@ -1,9 +0,0 @@
-
-package fenster;
-public class Fenster {
-    public static void main(String[] args) {
-        MeinFenster mf=new MeinFenster();
-        mf.setVisible(true);
-        mf.setTitle("Farbwahl");
-    }
-}
