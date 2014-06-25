1e90d5420dd3422f41de1f137357d74e478007f4
srithj <srithj@IR1-PC-19-W7.schule.local>
2014-06-25 16:58:06 +0200

    Änderung

---
+++ Fenster/src/fenster/Fenster.java
@@ -0,0 +1,9 @@
+
+package fenster;
+public class Fenster {
+    public static void main(String[] args) {
+        MeinFenster mf=new MeinFenster();
+        mf.setVisible(true);
+        mf.setTitle("Farbwahl");
+    }
+}
---
+++ Informatikprojekt KS2/src/informatikprojekt/ks2/InformatikprojektKS2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package informatikprojekt.ks2;
+
+/**
+ *
+ * @author BaumfS
+ */
+public class InformatikprojektKS2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        IPKS2_Fenster Fenster=new IPKS2_Fenster();
+        Fenster.setVisible(true);
+    }
+}
